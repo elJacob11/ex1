554d05ed706860c8eaa0b1db9714dd374e96260f
Michał Kowalkowski <kowalkowski.michal@gmail.com>
2022-07-08 15:06:39 +0200

    extract code + linting
    added new component

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,26 +1,44 @@
 import React from 'react';
 import Task from './Task';
 
-const TaskList = (props) => {
-    const active = props.tasks.filter(task => task.active);
-    const done = props.tasks.filter(task => !task.active);
-    const activeTasks = active.map(task => <Task key={task.id} task={task} delete={props.delete}
-        change={props.change} />)
-        const doneTasks = done.map(task => <Task key={task.id} task={task} delete={props.delete}
-            change={props.change} />)
+const TaskListFilter = ({ filter, label, tasks, delete, change, ... props }) => {
+    const filteredTasks = tasks.filter(filter);
+    return (
+        <div {...props}>
+            <h2>{label} <em>({filteredTasks.length})</em><</h2>
+            {filteredTasks.map(task => (
+                <Task 
+                    key={task.id} 
+                    task={task} 
+                    delete={delete} 
+                    change={change} 
+                />
+            ))}
+        </div>
+    )
+};
+
+const TaskList = ({ tasks, delete, change }) => {
     return ( 
         <>
-        <div className='active'>
-            <h2>Zadania do zrobienia</h2>
-            {activeTasks}
-        </div>
-        <hr/>
-        <div className='done'>
-        <h2>Zadania zrobione <em>({done.length})</em></h2>
-        {doneTasks}
-        
-        </div>
-    </>
+            <TaskListFilter 
+                className="active" 
+                label={'Zadania do zrobienia'} 
+                tasks={tasks} 
+                filter={task => task.active} 
+                delete={delete} 
+                change={change} 
+            />
+            <hr/>
+            <TaskListFilter 
+                className="done" 
+                label={'Zadania zrobione'} 
+                tasks={tasks} 
+                filter={task => !task.active} 
+                delete={delete} 
+                change={change} 
+            />
+        </>
      );
 }
  
